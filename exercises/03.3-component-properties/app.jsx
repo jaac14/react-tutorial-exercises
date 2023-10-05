378856e76bf1901a24ada78e46c754353cb91dbd
Jose Avendaño <124366885+jaac14@users.noreply.github.com>
2023-10-05 23:29:05 +0000

    ejercicios terminados

--- exercises/03.3-component-properties/app.jsx
+++ exercises/03.3-component-properties/app.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropType from "prop-types";
 
-const BootstrapCard = props => {
+const BootstrapCard = (props) => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
+			<img
+				className="card-img-top"
+				src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+				alt="Card image cap"
+			/>
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
+				<p className="card-text">{props.description}</p>
 				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
 					Go to wikipedia
 				</a>
@@ -18,9 +22,22 @@ const BootstrapCard = props => {
 	);
 };
 BootstrapCard.propTypes = {
-	title: PropType.string
-		// 2) add here the new properties into the proptypes object
+	title: PropType.string,
+	// 2) add here the new properties into the proptypes object
+	imageUrl: PropType.string,
+	description: PropType.string,
+	buttonUrl: PropType.string,
+	buttonLabel: PropType.string,
 };
 
-
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
+ReactDOM.render(
+	<BootstrapCard
+		title="Bob Dylan"
+		imageUrl="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+		buttonLabel="Go to wikipedia"
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+	/>,
+	document.querySelector("#myDiv")
+);
